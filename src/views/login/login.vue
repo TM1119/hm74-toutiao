38bd42a47cd9fce3ff68e459b3afb3832cf862e6
tm <13121039@qq.com>
2019-07-11 23:57:10 +0800

    登录设置

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,40 +1,100 @@
 <template>
-<div class="login-container">
- <el-card class="login-box">
-   <img src="../../assets/images/logo_index.png" alt="">
+  <div class="login-container">
+    <el-card class="login-box">
+      <img src="../../assets/images/logo_index.png" alt />
+      <el-form ref="loginForm" :status-icon="true" :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true"><span>我已阅读并同意</span>用户协议<span>和</span>隐私条款</el-checkbox>
+        </el-form-item>
 
- </el-card>
+        <el-form-item>
+          <el-button style="width:100%" type="primary" @click="login()">登录</el-button>
+        </el-form-item>
 
-</div>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+  // 校验手机号
+    const checkMobile = (rule, value, callback) => {
+      // 校验逻辑 把value拿出来进行手机的格式校验
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机格式不正确'))
+      }
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位数字', trigger: 'blur' }
+        ]
+      }
+    }
+    // markdown preview
+  },
+  methods: {
+    login () {
+      // 对整个表单进行校验
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then(res => {
+              // console.log(res)
+              this.$router.push('/')
+            })
+            .catch(() => {
+              // 错误提示
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
+    }
+  }
 }
+
 </script>
 
 <style lang="less">
-.login-container{
+.login-container {
   width: 100%;
   height: 100%;
   position: absolute;
-  left:0;
+  left: 0;
   top: 0;
   background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
-  .login-box{
+  .login-box {
     width: 400px;
-    height: 300px;
+    height: 340px;
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-50%,-50%);
-    img{
+    transform: translate(-50%, -50%);
+    img {
       display: block;
       width: 200px;
       margin: 10px auto;
     }
-
   }
 }
 </style>
