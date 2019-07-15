9965c9ae71734ac3b047aa9c04c9a5f313899c1a
tm <13121039@qq.com>
2019-07-16 00:44:56 +0800

    赛选和内容显示

--- src/views/article/article.vue
+++ src/views/article/article.vue
@@ -1,60 +1,96 @@
 <template>
   <div class="article-container">
-  <!-- 筛选区域 -->
-  <el-card>
-    <div slot="header">
-      <my-bread>内容管理</my-bread>
-    </div>
-    <!-- 筛选表单 -->
-    <el-form :model="reqParams" size="small" label-width="80px">
-      <el-form-item label="状态:">
+    <!-- 筛选区域  明天删了 -->
+    <el-card>
+      <div slot="header">
+        <my-bread>内容管理</my-bread>
+      </div>
+      <!-- 筛选表单 -->
+      <el-form :model="reqParams" size="small" label-width="80px">
+        <el-form-item label="状态:">
           <el-radio-group v-model="reqParams.status">
-          <el-radio :label="null">全选</el-radio>
-          <el-radio :label="6">草稿</el-radio>
-          <el-radio :label="9">待审核</el-radio>
-          <el-radio :label="9">审核通过</el-radio>
-          <el-radio :label="9">审核失败</el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="频道:">
-         <el-select v-model="reqParams.channel_id" placeholder="请选择">
+            <el-radio :label="null">全选</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道:">
+          <el-select v-model="reqParams.channel_id" placeholder="请选择">
             <el-option
               v-for="item in  channelOptions"
               :key="item.id"
               :label="item.name"
-              :value="item.id">
-            </el-option>
+              :value="item.id"
+            ></el-option>
           </el-select>
-      </el-form-item>
-      <el-form-item label="日期:">
-         <el-date-picker
-      v-model="dateValues"
-      type="daterange"
-      range-separator="至"
-      start-placeholder="开始日期"
-      end-placeholder="结束日期">
-    </el-date-picker>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary">筛选</el-button>
-      </el-form-item>
-    </el-form>
-  </el-card>
-  <!-- 结果区域 -->
-  <el-card>
-    <my-test>
-      <!-- <template slot="content" slot-scope="scope"> 内容1{{scope.test}} </template> -->
+        </el-form-item>
+        <el-form-item label="日期:">
+          <el-date-picker
+            value-format="yyyy-MM-dd"
+            @change="changeDate"
+            v-model="dateValues"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="search()">筛选</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- 结果区域 -->
+    <el-card>
+      <!-- <my-test>
+      <template slot="content" slot-scope="scope"> 内容1{{scope.test}} </template>
       <template v-slot:content="scope"> 内容1{{scope.test}} </template>
      <template slot="footer">底部1</template>
-       </my-test>
-  </el-card>
+      </my-test>-->
+      <div slot="header">
+        根据筛选条件共查询到
+        <b>0</b>条结果:
+      </div>
+      <el-table :data="articles">
+        <el-table-column label="封面">
+          <template slot-scope="scope">
+
+            <el-image :src="scope.row.cover.images[0]" style="width:100px;height:75px">
+                <div slot="error" >
+                 <img src="../../assets/images/error.gif" width="100" height="75" alt="">
+                </div>
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column label="标题" prop="title"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+              <el-tag v-if="scope.row.status===0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status===1">待审核</el-tag>
+              <el-tag v-if="scope.row.status===2" type="success">审核通过</el-tag>
+              <el-tag v-if="scope.row.status===3" type="warning">审核失败</el-tag>
+              <el-tag v-if="scope.row.status===4" type="danger">已删除</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="发布时间" prop="pubdate"></el-table-column>
+        <el-table-column label="操作" width="120px">
+          <template slot-scope="">
+            <el-button icon="el-icon-edit" plain type="primary" circle></el-button>
+            <el-button icon="el-icon-delete" plain type="danger" circle></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="box">
+        <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
-
 export default {
-
   data () {
     return {
       reqParams: {
@@ -63,16 +99,53 @@ export default {
         begin_pubdate: null,
         end_pubdate: null
       },
+      // 频道的选项数组
       channelOptions: [{ name: 'java', id: 1 }],
       // 日期数据
-      dateValues: []
+      dateValues: [],
+      // 文章列表数据
+      articles: []
+    }
+  },
+  created () {
+    // 获取频道数据
+    this.getChannelOptions()
+    // 获取文章列表数据
+    this.getArticle()
+  },
+  methods: {
+    // 搜索
+    search () {
+      this.getArticle()
+    },
+    // 选择时间处理函数
+    changeDate (value) {
+      this.reqParams.begin_pubdate = value[0]
+      this.reqParams.end_pubdate = value[1]
+    },
+    // 获取频道数据
+    async getChannelOptions () {
+      const {
+        data: { data }
+      } = await this.$http.get('channels')
+      this.channelOptions = data.channels
+    },
+    async getArticle () {
+      const {
+        data: { data }
+      } = await this.$http.get('articles', { params: this.reqParams })
+      this.articles = data.results
     }
   }
 }
 </script>
 
 <style lang="less">
-  .el-card{
-   margin-bottom: 20px;
-  }
+.el-card {
+  margin-bottom: 20px;
+}
+.box {
+  text-align: center;
+  margin-top: 20px;
+}
 </style>
--- src/views/welcome/welcome.vue
+++ src/views/welcome/welcome.vue
@@ -6,9 +6,7 @@
 
 <script>
 export default {
-  created () {
-    this.$http.get('http://ttapi.research.itcast.cn/mp/v1_0/articles').then(res => console.log(res.data))
-  }
+
 }
 </script>
 
